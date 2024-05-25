b646a4f015e34c593ab1d89ca27f5234dcb66d42
Ramon Fossa Chiesi <ramon.fossa@gmail.com>
2024-05-25 19:44:02 -0300

    Init oop with java

---
+++ java/oop.java
@@ -0,0 +1,31 @@
+class OOP {
+    
+    public static void main(String[] args) {
+        /*
+        Pilares da POO:
+
+        - Abstração;
+        - Encapsulamento;
+        - Herança;
+        - Polimorfismo;
+
+        Abstração:
+        - Definir o que do mundo real será trazido para o seu sistema.
+
+         */
+    }
+
+}
+
+class Casa {
+    // Atributos ou propriedades:
+    String cor;
+    String endereco;
+    int numero;
+
+    // Métodos 
+
+    public void ligarLuzes() {
+
+    }
+}
